1d801b09b3cfb8b9c7a539c6c273d150391f8275
Ericka Stenman <elstenman@gmail.com>
2017-10-16 18:07:22 -0700

    Committed devise config change

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,6 +14,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'app78145809@heroku.com'
+  # config.mailer_sender = 'elstenman@gmail.com'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
